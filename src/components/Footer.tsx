b92b9d49745d24d2be003196ddc7abebe21c8dee
timonen87 <timmonen.as@gmail.com>
2024-01-28 01:30:35 +0300

    fix h1 style 1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,11 @@ interface FooterProps {}
 const Footer: FC<FooterProps> = ({}) => {
   return (
     <footer className="w-full bg-white p-8  mt-4">
-        <div className='container max-w-7xl grid-cols-1 mx-auto grid sm:grid-cols-5 gap-4 '>
-        	<div className='col-span-4'>&copy; GetSpy.ru 2020 - 2023 Copyright</div>
-            <a className="text-sm col-auto" href="#">
-                Все права защищены
-            </a>
+      <div className="container max-w-7xl grid-cols-1 mx-auto grid sm:grid-cols-5 gap-4 ">
+        <div className="col-span-4">&copy; GetSpy.ru 2021 - 2024 Copyright</div>
+        <a className="text-sm col-auto" href="/">
+          Все права защищены
+        </a>
       </div>
     </footer>
   );
